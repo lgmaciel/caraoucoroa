9e1bbf8b3e17be4ebccd1ce0346a43369720747c
lgmaciel <lgmaciel@email>
2025-11-17 16:38:21 -0300

    primeira versão funcional

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-divMoeda = document.querySelector("#moeda");
+const divMoeda = document.querySelector("#moeda");
+const btCara = document.querySelector("#btCara")
+const btCoroa = document.querySelector("#btCoroa")
+const txPontos = document.querySelector("#txPontos")
+const txAposta = document.querySelector("#txAposta")
 
 //função que retorna 0="Cara" ou 1="Coroa"
 function tirarCaraOuCoroa() {
@@ -10,10 +14,45 @@ function tirarCaraOuCoroa() {
     return moeda[ladoSorteado]
 }
 
-function jogarMoeda() {
+
+divMoeda.addEventListener("click", () => {
     resultado = tirarCaraOuCoroa()
     divMoeda.innerText = resultado
-}
+})
+
+btCara.addEventListener("click", ()=>{
+    valorAposta = Number(txAposta.value)
+    
+    resultado = tirarCaraOuCoroa()
+    divMoeda.innerText = resultado
+    // pontuação
+    if (resultado === "Cara") {
+        txPontos.value = "💰" + (Number(txPontos.value.slice(2)) + valorAposta)
+    } else {
+        txPontos.value = "💰" + (Number(txPontos.value.slice(2)) - valorAposta)
+    }
+})
+
+btCoroa.addEventListener("click", ()=>{
+    valorAposta = Number(txAposta.value)
+
+    resultado = tirarCaraOuCoroa()
+    divMoeda.innerText = resultado
+    // pontuação
+    if (resultado === "Coroa") {        
+        txPontos.value = "💰" + (Number(txPontos.value.slice(2)) + valorAposta)
+    } else {
+        txPontos.value = "💰" + (Number(txPontos.value.slice(2)) - valorAposta)
+    }
+})
 
 
-divMoeda.addEventListener("click", jogarMoeda)
+txAposta.addEventListener("change", () => {
+    // o valor da aposta que está sendo feita
+    // ultrapassa os pontos que ganhei?
+    if(
+        Number(txAposta.value) >= Number(txPontos.value.slice(2))
+    ) {
+        txAposta.value = txPontos.value.slice(2)
+    }
+})
